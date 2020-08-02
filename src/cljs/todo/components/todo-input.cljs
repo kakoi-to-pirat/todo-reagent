107bd3874b3b1f4405d0cd1d51d2c4e2d938ad13
Какой-то Пират <kakoi-to.pirat@yandex.ru>
2020-08-02 19:21:29 +0300

    format code

--- src/cljs/todo/components/todo-input.cljs
+++ src/cljs/todo/components/todo-input.cljs
@@ -11,8 +11,8 @@
 (def escape-key 27)
 
 (defn add-todo [] ((store/add-todo @input-value)
-                  (helpers/log (str "enter: " @input-value))
-                  (reset! input-value "")))
+                   (helpers/log (str "enter: " @input-value))
+                   (reset! input-value "")))
 
 (defn on-enter [] (if (= @input-value "") nil (add-todo)))
 
